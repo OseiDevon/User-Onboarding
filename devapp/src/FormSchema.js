36a04e62a69c2517d9fd1441a9016642994e9487
Devon Osei <oseidevon@gmail.com>
2021-03-24 21:34:13 -0400

    ummm

---
+++ devapp/src/FormSchema.js
@@ -0,0 +1,16 @@
+import * as yup from 'yup'
+const FormSchema = yup.object().shape({
+    name: yup.string()
+        .trim()
+        .required('Name is required.')
+        .min(3, 'Name must be 3 characters long'),
+    email: yup.string()
+        .email('Valid email address')
+        .required('Email is required'),
+    password: yup.string()
+        .min(8,'Password must be 8 characters long')
+        .required('Password is required.'),
+
+    termsOfService: yup.boolean().oneOf([true],'Must Accept Terms of Service'),
+    userAgreement: yup.boolean().oneOf([false],'Must accept Terms of Service'),
+})
